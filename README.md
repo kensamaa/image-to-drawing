20df9e6dc9e7bc80877882c5843fe0e2fc3c2f11
kensamaa <amineab203@gmail.com>
2020-10-15 17:00:00 +0100

    updating the readme

---
+++ README.md
@@ -0,0 +1,18 @@
+
+<h2>steps to turn picture to draw</h2>
+
+<p>turn the picture to gray</p>
+
+![](i/img/grayImage.jpg)
+<p>invert the picture</p>
+<p>blur the image</p>
+
+![](img/invertedImage.jpg)
+<p>devide the picture </p>
+
+![](img/drawingImage.jpg)
+<p>modify some code and you get some cool effects</p>
+
+
+![](img/logoImage.jpg)
+
--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import cv2
 img = cv2.imread("3.jpg")
 height, width = img.shape[0:2]
 
-#                cv2.getRotationMatrix2D(center, angle, scale)
 rotationMatrix = cv2.getRotationMatrix2D((width/2, height/2), 90, .5)
 
 
@@ -20,17 +19,16 @@ blur=cv2.blur(inverted_img,(5,5))
 
 img_blend= cv2.divide(gray, 255-blur, scale=256)
 f=cv2.cvtColor(img_blend, cv2.COLOR_GRAY2BGR)
-logo= cv2.divide(gray, 155-blur, scale=256)
+logo= cv2.divide(img, 155-blur, scale=256)
+#show the pictures
 cv2.imshow('logo Image', logo)
 cv2.imshow('gray Image', gray)
 cv2.imshow('inverted Image', inverted_img)
 cv2.imshow('drawing Image', f)
-
+#save the pictures
 cv2.imwrite('./img/logoImage.jpg', logo) 
 cv2.imwrite('./img/grayImage.jpg', gray) 
 cv2.imwrite('./img/invertedImage.jpg', inverted_img) 
 cv2.imwrite('./img/drawingImage.jpg', f) 
 
 cv2.waitKey(0)
-'''cv2.imshow('Original Image', img) 
-cv2.waitKey(0)'''
